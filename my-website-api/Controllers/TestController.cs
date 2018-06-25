3c509b10137ce189fcf8b1a76d0340fd483ef504
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-06-26 07:47:30 +1000

    Renamed ValuesController to TestController and add a test post

---
+++ my-website-api/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_website_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+
+        // POST api/test/testpost
+        [HttpPost("testpost")]
+        public void Testpost([FromBody]string value)
+        {
+         //Change string to a RDTO
+         //Change void to a SDTO
+        }
+    }
+}
