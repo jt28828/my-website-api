8beec8df823529b688617bdc2b1e66e7145535ca
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-07-03 18:20:42 +1000

    Added an example DTO for post

--- my-website-api/Controllers/TestController.cs
+++ my-website-api/Controllers/TestController.cs
@@ -6,16 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace my_website_api.Controllers
 {
+
+    public class myDTO
+    {
+        public string name;
+        public string pass;
+        public int count;
+    }
+
     [Route("api/[controller]")]
     public class TestController : Controller
     {
 
         // POST api/test/testpost
         [HttpPost("testpost")]
-        public void Testpost([FromBody]string value)
+        public myDTO Testpost([FromBody]myDTO value)
         {
-         //Change string to a RDTO
-         //Change void to a SDTO
+            Console.WriteLine(value.name);
+            value.count++;
+            return value;
+            //Change string to a RDTO
+            //Change void to a SDTO
         }
     }
 }
